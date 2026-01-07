9bcda3ddcf5119929a12752ae7831b1c91f605ed
LucasDebeve <lucas.debeve2003@gmail.com>
2026-01-07 18:41:28 +0100

    style: front

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,10 +9,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('dashboard/index.html.twig');
+    }
+
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(): Response
+    {
+        return $this->render('dashboard/profile.html.twig');
+    }
+
+    #[Route('/', name: 'app_landing')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
-        ]);
+        return $this->render('dashboard/landing.html.twig');
     }
 }
